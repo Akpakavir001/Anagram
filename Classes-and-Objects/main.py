9e8702065020c2f0da3677d679bcd126f0937bf6
Okwumuo Basil Moses <basildaprof.bd@gmail.com>
2022-06-09 17:04:50 +0100

    Don't give up

---
+++ Classes-and-Objects/main.py
@@ -0,0 +1,31 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self,name:str,age:int,tracks:list, score:float):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+
+    print("\nNew Student Information:\n")
+
+    def change_name(self,new_name):
+        self.name = new_name
+        print("Name: " + new_name.title())
+
+    def change_age(self,new_age):
+        self.age = new_age
+        print("Age: " + str(new_age))
+
+    def add_track(self,new_track):
+        self.tracks = self.tracks.append(new_track)
+        print("Tracks: " + new_track)
+
+    def get_score(self,new_score):
+        self.score = new_score
+        print("Score: " + str(new_score))
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track('["UI/UX"]')
+Bob.get_score(20.90)
